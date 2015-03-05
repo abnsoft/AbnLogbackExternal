65f30e3de3bd1b900119e2d11375ec34d04f95c1
AB.Nikitin <AB.Nikitin@bk.ru>
2015-03-05 16:07:25 +0400

    final commit

--- README.txt
+++ README.txt
@@ -1,6 +1,29 @@
 
-Tasks : 
+Logback, outside of war, external using logback, logback outside war.
 
-1. Create possibility of using some context-argument parameter which is defined outside (as JVM argument).
+This is extends of ServletContextListener. 
+Using this listener allows you to use in web application Logback config file in any place of filesystem. You should not place now logback.xml either only inside of web app or classpath. 
 
-  
+The main goal of this library is changing the logging on the fly without restarting and changing any files in web applicat
+
+ion. It is based on: 
+
+Servlets 2.5, 
+spring-web 4.1.5.RELEASE, 
+logback-classic 
+
+
+You have to define 2 things : 
+
+1. In JVM arguments (jvm or Tomcat vm arguments) define some arguments. I.e. 
+-DmyConfigDir="d:/my/config/logback/" . 
+Where "d:/my/config/logback/" is folder where you will put your config file. 
+
+2. In web.xml file create new context-param : 
+ <context-param>
+     <param-name>LogbackConfigFilename</param-name>
+     <param-value>file:${myConfigDir}/logback.xml</param-value>
+ </context-param>
+ 
+
+ 
--- src/main/java/info/abnsoft/java/servlet/utils/LogbackExternalListener.java
+++ src/main/java/info/abnsoft/java/servlet/utils/LogbackExternalListener.java
@@ -1,16 +1,12 @@
 package info.abnsoft.java.servlet.utils;
 
-import info.abnsoft.java.servlet.exception.NoFoundServletContext;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.ResourceUtils;
 //import java.util.logging.LogManager;
@@ -21,9 +17,43 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
 
+import com.sun.tools.javac.util.Assert;
+
 //import ch.qos.logback.classic.LoggerContext;
 
 /**
+ * This is extends of {@link ServletContextListener}. <br />
+ * Using this listener allows you to use in web application Logback config file in any place of filesystem.
+ * You should not place now logback.xml either only inside of web app or classpath. <br />
+ * <br />
+ * The main goal of this library is changing the logging on the fly without restarting and changing any files
+ * in web applicat<br />
+ * <br />
+ * ion. It is based on:
+ * <ul>
+ * <li>Servlets 2.5,
+ * <li>spring-web 4.1.5.RELEASE,
+ * <li>logback-classic <br />
+ * </ul>
+ * <br />
+ * You have to define 2 things : <br />
+ * <br />
+ * 1. In JVM arguments (jvm or Tomcat vm arguments) define some arguments. I.e. <br />
+ * -DmyConfigDir="d:/my/config/logback/" . <br />
+ * Where "d:/my/config/logback/" is folder where you will put your config file. <br />
+ * <br />
+ * 2. In web.xml file create new context-param :
+ * 
+ * <pre>
+ * &lt;context-param&gt;
+ *     &lt;param-name&gt;LogbackConfigFilename&lt;/param-name&gt;
+ *     &lt;param-value&gt;file:${myConfigDir}/logback.xml&lt;/param-value&gt;
+ * &lt;/context-param&gt;
+ * </pre>
+ * 
+ * 
+ * 
+ * @author annik
  */
 public class LogbackExternalListener implements ServletContextListener {
 
@@ -34,23 +64,19 @@ public class LogbackExternalListener implements ServletContextListener {
 
     public static final String LOGBACK_CONFIG_FILE_NAME_DEFAULT = "logback.xml";
 
-    private static final Logger LOG = LoggerFactory.getLogger( LogbackExternalListener.class );
-
     /*
      * (non-Javadoc)
      * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
      */
     public void contextInitialized( ServletContextEvent sce ) {
 
-        LOG.debug( this.getClass().getName() + " ~ Context initialized" );
+        Assert.checkNonNull( sce );
 
         ServletContext defaultServletContext = sce.getServletContext();
+        defaultServletContext.log( this.getClass().getName() + " ~ Context initialized" );
 
-        if ( defaultServletContext == null ) {
-            throw new NoFoundServletContext();
-        }
         if ( defaultServletContext.getInitParameter( LOGBACK_CONFIG_FILE_NAME ) == null ) {
-            LOG.warn( "There is defined " + this.getClass().getName()
+            defaultServletContext.log( "There is defined " + this.getClass().getName()
                     + ", but not found <context-param><param-name>" + LOGBACK_CONFIG_FILE_NAME
                     + "</param-name>... (in web.xml). Will try to use default logback.xml." );
 
@@ -59,17 +85,19 @@ public class LogbackExternalListener implements ServletContextListener {
             String logbackConfigurationFile = "";
 
             String contextParamLogback = defaultServletContext.getInitParameter( LOGBACK_CONFIG_FILE_NAME );
-            LOG.debug( "Found param-name=`{}` , value=`{}`", LOGBACK_CONFIG_FILE_NAME, contextParamLogback );
+            defaultServletContext.log( "Found param-name=`{" + LOGBACK_CONFIG_FILE_NAME + "}` , value=`{"
+                    + contextParamLogback + "}`" );
 
             if ( contextParamLogback.matches( ".*(\\$\\{.*\\}).*" ) ) {
                 String contextName =
                         contextParamLogback.substring( contextParamLogback.indexOf( "${" ),
                                 contextParamLogback.indexOf( "}" ) + 1 );
                 contextName = contextName.length() < 3 ? "   " : contextName;
-                LOG.warn(
-                        "It seems there is not defined valid name VM arguments for param-name=`{}`! You have to"
-                                + " use in VM arguments of Application server or JVM  … -D{}=/path/to/dir … ",
-                        contextName, contextName.substring( 2, contextName.length() - 1 ) );
+                defaultServletContext
+                        .log( "It seems there is not defined valid name VM arguments for param-name=`{"
+                                + contextName + "}`! You have to"
+                                + " use in VM arguments of Application server or JVM  … -D{"
+                                + contextName.substring( 2, contextName.length() - 1 ) + "}=/path/to/dir … " );
             }
 
             try {
@@ -85,13 +113,11 @@ public class LogbackExternalListener implements ServletContextListener {
 
                 defaultServletContext.log( "Initializing Logback from [" + logbackConfigurationFile + "]" );
 
-                InputStream configResource =
-                        defaultServletContext.getResourceAsStream( logbackConfigurationFile );
-
                 initLogback( ResourceUtils.getFile( logbackConfigurationFile ) );
 
             } catch (FileNotFoundException e) {
-                LOG.error( "Logback configuration file `{}` not found.", contextParamLogback );
+                defaultServletContext.log( "Logback configuration file `{" + contextParamLogback
+                        + "}` not found." );
             }
 
         }
@@ -104,17 +130,20 @@ public class LogbackExternalListener implements ServletContextListener {
      */
     public void contextDestroyed( ServletContextEvent sce ) {
 
-        LOG.debug( "contex DESTROYED" );
+        sce.getServletContext().log( "contex DESTROYED" );
+
         ( (LoggerContext) LoggerFactory.getILoggerFactory() ).stop();
 
     }
 
     /**
-     * Initialize Logback with given file,
+     * Initialize Logback with given {@link File}
      * 
      * @param configPath
      */
     public static void initLogback( File configResource ) {
+
+        Assert.checkNonNull( configResource );
 
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
         context.reset();
@@ -126,23 +155,18 @@ public class LogbackExternalListener implements ServletContextListener {
 
         } catch (JoranException e) {
             throw new RuntimeException( "Cannot initialize logback with given file `"
-                    + configResource.toString() + "` ", e );
+                    + configResource.getAbsolutePath() + "` ", e );
         }
     }
 
+    /**
+     * Initialize Logback with given file path.
+     * 
+     * @param configPath
+     */
     public static void initLogback( String configPath ) {
 
-        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        context.reset();
-
-        try {
-            JoranConfigurator configurator = new JoranConfigurator();
-            configurator.setContext( context );
-            configurator.doConfigure( configPath );
-
-        } catch (JoranException e) {
-            throw new RuntimeException( "Cannot initialize logback with given file `" + configPath + "` ", e );
-        }
+        initLogback( new File( configPath ) );
     }
 
 }
